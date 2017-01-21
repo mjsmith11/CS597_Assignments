4b55effc2057422d87d9b0557c516bae08c4b882
Matt Smith <mjsmith92@Outlook.com>
2017-01-21 13:41:38 -0500

    Add Part 3

---
+++ CS597_Project1/CS597_Project1/GradeCalculator.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GradeCalculator.aspx.cs" Inherits="CS597_Project1.GradeCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Grade Calculator</h1>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>
+                <asp:Label ID="lblName" runat="server" Text="Student Name"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxName" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblQuiz" runat="server" Text="Quiz Score"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxQuiz" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAssignment" runat="server" Text="Assignment Score"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxAssignment" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblMidterm" runat="server" Text="Midterm Exam Score"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxMidterm" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblFinal" runat="server" Text="Final Exam Score"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxFinal" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblGrade" runat="server" Text="Grade"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxPercentage" runat="server" ReadOnly="true"></asp:TextBox> &nbsp; <asp:TextBox ID="tbxLetter" runat="server" ReadOnly="true"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS597_Project1/CS597_Project1/GradeCalculator.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS597_Project1
+{
+    public partial class GradeCalculator : System.Web.UI.Page
+    {
+        private bool fieldsValid;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double quizScore, assignmentScore, midtermScore, finalExamScore, averageScore;
+            string letterGrade;
+
+            fieldsValid = true;
+            quizScore = getScore(tbxQuiz);
+            assignmentScore = getScore(tbxAssignment);
+            midtermScore = getScore(tbxMidterm);
+            finalExamScore = getScore(tbxFinal);
+
+            if(fieldsValid)
+            {
+                averageScore = (quizScore * 0.15) + (assignmentScore * 0.40) + (midtermScore * 0.20) + (finalExamScore * 0.25);
+                letterGrade = getLetter(averageScore);
+                tbxPercentage.Text = averageScore.ToString();
+                tbxLetter.Text = letterGrade.ToString();
+
+            }
+            else
+            {
+                tbxPercentage.Text = "Invalid";
+                tbxLetter.Text = "Input";
+            }
+
+        }
+
+        /// <summary>
+        /// Retrieves the text from the passed control and validates it as a score. 
+        /// Sets the class member fieldsValid to false if validation fails
+        /// </summary>
+        /// <param name="control">Control to retrieve text from</param>
+        /// <returns>The score from the control if validation is successful. -1 otherwise</returns>
+        private double getScore(TextBox control)
+        {
+            double score;
+            try
+            {
+                score = Double.Parse(control.Text);
+            }
+            catch(FormatException e)
+            {
+                fieldsValid = false;
+                return -1;
+            }
+            if(score>=0)
+            {
+                return score;
+            }
+            else
+            {
+                fieldsValid = false;
+                return -1;
+            }
+        }
+
+        public string getLetter(double score)
+        {
+            if (score >= 93)
+                return "A";
+            else if (score >= 90)
+                return "A-";
+            else if (score >= 87)
+                return "B+";
+            else if (score >= 83)
+                return "B";
+            else if (score >= 80)
+                return "B-";
+            else if (score >= 77)
+                return "C+";
+            else if (score >= 73)
+                return "C";
+            else if (score >= 70)
+                return "C-";
+            else if (score >= 67)
+                return "D+";
+            else if (score >= 63)
+                return "D";
+            else if (score >= 60)
+                return "D-";
+            else
+                return "F";
+        }
+    }
+}
