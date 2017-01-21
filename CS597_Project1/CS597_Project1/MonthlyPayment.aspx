ad1ffa17f00cdeff47bf87b41ccddda90fedf67a
Matt Smith <mjsmith92@Outlook.com>
2017-01-21 14:17:27 -0500

    Project 1 Part 4

---
+++ CS597_Project1/CS597_Project1/MonthlyPayment.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MonthlyPayment.aspx.cs" Inherits="CS597_Project1.MonthlyPayment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Car Payment Calculator</h1>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>
+                <asp:Label ID="lblPrice" runat="server" Text="Car Price"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxPrice" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblDownPayment" runat="server" Text="Down Payment"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxDownPayment" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblAPR" runat="server" Text="Annual Percentage Rate"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxAPR" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblYears" runat="server" Text="Years"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxYears" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblMonthlyPayment" runat="server" Text="Monthly Payment"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="tbxMonthlyPayment" ReadOnly="true" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS597_Project1/CS597_Project1/MonthlyPayment.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS597_Project1
+{
+    public partial class MonthlyPayment : System.Web.UI.Page
+    {
+        private bool fieldsValid;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double price, downPayment, APR, years, monthlyPayment, i, n;
+
+            fieldsValid = true;
+            price = getPositiveDecimal(tbxPrice);
+            downPayment = getPositiveDecimal(tbxDownPayment);
+            APR = getPositiveDecimal(tbxAPR);
+            years = getPositiveDecimal(tbxYears);
+
+            if (fieldsValid)
+            {
+                i = APR / 1200;
+                n = years * 12;
+
+                monthlyPayment = ((price - downPayment) * i * Math.Pow(1 + i, n)) / (Math.Pow(1 + i, n) - 1);
+                tbxMonthlyPayment.Text = monthlyPayment.ToString("C");
+            }
+            else
+            {
+                tbxMonthlyPayment.Text = "Invalid Input";
+            }
+        }
+        /// <summary>
+        /// Retrieves the text from the passed control and validates it as a positive double. 
+        /// Sets the class member fieldsValid to false if validation fails
+        /// </summary>
+        /// <param name="control">Control to retrieve text from</param>
+        /// <returns>The value from the control if validation is successful. -1 otherwise</returns>
+        private double getPositiveDecimal(TextBox control)
+        {
+            double value;
+            try
+            {
+                value = Double.Parse(control.Text);
+            }
+            catch(FormatException e)
+            {
+                fieldsValid = false;
+                return -1;
+            }
+
+            if(value >= 0)
+            {
+                return value;
+            }
+            else
+            {
+                fieldsValid = false;
+                return -1;
+            }
+        }
+    }
+};
