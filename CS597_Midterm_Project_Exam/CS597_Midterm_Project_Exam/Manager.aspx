2ac53fbec43df7c7ad08d097b6ca7a93b81a5596
Matt Smith <mjsmith92@outlook.com>
2017-02-25 16:36:01 -0500

    MidTerm-Add Manager Page

---
+++ CS597_Midterm_Project_Exam/CS597_Midterm_Project_Exam/Manager.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Manager.aspx.cs" Inherits="CS597_Midterm_Project_Exam.Manager" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 43px;
+        }
+        .auto-style2 {
+            width: 256px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Bug Assignment</h1>
+    <form id="form1" runat="server">
+    <div>
+    <table><tr>
+        <td>
+        <asp:Label ID="lblBug" runat="server" Text="Bug"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlBug" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlBug_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="lblDeveloper" runat="server" Text="Developer"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlDeveloper" runat="server">
+        </asp:DropDownList>
+    
+        <br />
+        <br />
+        <asp:Button ID="btnAssign" runat="server" Text="Assign" OnClick="btnAssign_Click" />
+    
+        <br />
+        <br />
+            </td>
+        <td class="auto-style1"></td>
+        <td class="auto-style2">
+            <h4>Bug Details</h4>
+            <asp:DetailsView ID="dvBugDisplay" runat="server" Height="50px" Width="252px">
+        </asp:DetailsView>
+        </td>
+        </tr></table>
+        <asp:Label ID="lblFeedback" runat="server" Text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS597_Midterm_Project_Exam/CS597_Midterm_Project_Exam/Manager.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace CS597_Midterm_Project_Exam
+{
+    public partial class Manager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null || Session["UserType"] == null)
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+            else if (!Session["UserType"].Equals("Manager"))
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if(!IsPostBack)
+            {
+                try
+                {
+                    OleDbHandler db = new OleDbHandler("MidTermCS");
+                    db.CreateCommand("SELECT Subject,BugID FROM Bugs");
+                    db.OpenConnection();
+                    OleDbDataReader rdr = db.GetDataReader();
+                    while(rdr.Read())
+                    {
+                        ddlBug.Items.Add(new ListItem(rdr["Subject"].ToString(), rdr["BugID"].ToString()));
+                    }
+
+                    rdr.Close();
+                    db.CloseConnection();
+
+                    db.CreateCommand("SELECT UserID, Name FROM Users WHERE Type='Developer'");
+                    db.OpenConnection();
+                    rdr = db.GetDataReader();
+                    while(rdr.Read())
+                    {
+                        ddlDeveloper.Items.Add(new ListItem(rdr["Name"].ToString(), rdr["UserID"].ToString()));
+                    }
+                    rdr.Close();
+                    db.CloseConnection();
+                }
+                catch(Exception ex)
+                {
+                    Response.Redirect("Login.aspx");
+                    return;
+                }
+                ddlBug_SelectedIndexChanged(null, null);
+            }
+        }
+
+        protected void btnAssign_Click(object sender, EventArgs e)
+        {
+            int developerID = -1;
+            int bugID = -1;
+            int retVal = 0;
+
+            lblFeedback.Text = "";
+            developerID = Int32.Parse(ddlDeveloper.SelectedValue);
+            bugID = Int32.Parse(ddlBug.SelectedValue);
+
+            try
+            {
+                OleDbHandler db = new OleDbHandler("MidTermCS");
+                db.CreateCommand("UPDATE Bugs SET AssignedTo=@d WHERE BugID=@b");
+                db.AddParameter("@d", developerID);
+                db.AddParameter("@b", bugID);
+                db.OpenConnection();
+                retVal = db.ExecuteNonQuery();
+                db.CloseConnection();
+            }
+            catch(Exception ex)
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            lblFeedback.Text = ddlBug.SelectedItem.Text + " assigned to " + ddlDeveloper.SelectedItem.Text;
+
+
+        }
+
+        protected void ddlBug_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int bugID = -1;
+            bugID = Int32.Parse(ddlBug.SelectedValue);
+
+            try
+            {
+                OleDbHandler db = new OleDbHandler("MidTermCS");
+                db.CreateCommand("SELECT * FROM Bugs WHERE BugID=@b");
+                db.AddParameter("@b", bugID);
+                DataTable dt = db.ExecuteDataAdapterQuery();
+                dvBugDisplay.DataSource = dt;
+                dvBugDisplay.DataBind();
+            }
+            catch(Exception ex)
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+    }
+}
