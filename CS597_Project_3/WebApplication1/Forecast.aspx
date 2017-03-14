570d1600c86b3789afb99c485925604d18ca9f9d
Matt Smith <mjsmith92@outlook.com>
2017-03-14 19:40:15 -0400

    Project 3: Set up web service

---
+++ CS597_Project_3/WebApplication1/Forecast.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Forecast.aspx.cs" Inherits="WebApplication1.Forecast" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Weather Forecast</h1>
+    <form id="form1" runat="server">
+        <div>
+        <asp:Label ID="lblZip" runat="server" Text="Zip Code(s)"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="tbxZip" runat="server" Width="173px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnSubmit" runat="server" Text="Get Forecast" OnClick="btnSubmit_Click" />
+        <br />
+        <asp:Label ID="lblErr" runat="server" Text=""></asp:Label>
+&nbsp;
+    
+    </div>
+        <div id="divForecast" runat="server">
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS597_Project_3/WebApplication1/Forecast.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace WebApplication1
+{
+    public partial class Forecast : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblErr.Text = "";
+            if (!validateZips(tbxZip.Text))
+            {
+                lblErr.Text = "Please enter one or more zipcodes separated by spaces";
+                return;
+            }
+            divForecast.InnerHtml = "<h3>"+getLatLong(tbxZip.Text) + "</h3>";
+        }
+
+        private bool validateZips(string zips)
+        {
+            
+            String[] zipList = zips.Split(' ');
+            foreach(string z in zipList)
+            {
+                if(z.Length != 5)
+                {
+                    return false;
+                }
+                int n;
+                if(!Int32.TryParse(z,out n))
+                {
+                    //it is not numeric
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string getLatLong(string zips)
+        {
+            //NWS.ndfdXMLPortTypeClient weather = new NWS.ndfdXMLPortTypeClient();
+            //return weather.LatLonListZipCode(zips);
+            // return weather.NDFDgenByDay(Convert.ToDecimal(40.4755), Convert.ToDecimal(-86.1331), DateTime.Today, "5", NWS.unitType.e, NWS.formatType.Item12hourly);
+            //NWS.ndfdXML weather = new NWS.ndfdXML();
+            // return weather.NDFDgenByDay(86, 45, DateTime.Today, "5", NWS.unitType.e, NWS.formatType.Item24hourly);
+            noaa.ndfdXML weather = new noaa.ndfdXML();
+            return weather.LatLonListZipCode(zips);
+        }
+    }
+}
