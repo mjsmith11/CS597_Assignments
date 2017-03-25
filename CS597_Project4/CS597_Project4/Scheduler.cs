bb3403f37dc666469c3ba3d45e4a941e339a8bcb
Matt Smith <mjsmith92@outlook.com>
2017-03-25 14:50:49 -0400

    Project 4: Add scheduling algorithm

--- CS597_Project4/CS597_Project4/Scheduler.cs
+++ CS597_Project4/CS597_Project4/Scheduler.cs
@@ -9,7 +9,7 @@ namespace CS597_Project4
     {
         private List<List<Course>> sections { get; set; }
         public List<Course> schedule { get; set; }
-        public bool resultFound { get; set; }
+        public bool scheduleFound { get; set; }
 
         /// <summary>
         /// Constructor
@@ -19,7 +19,71 @@ namespace CS597_Project4
         {
             this.sections = sections;
             this.schedule = new List<Course>();
-            resultFound = true;
+            scheduleFound = true;
+        }
+
+        /// <summary>
+        /// initializes variables and starts the recursive search
+        /// </summary>
+        /// <returns></returns>
+        public bool searchForSchedule()
+        {
+            scheduleFound = false;
+            schedule.Clear();
+            return recursiveSearch();
+        }
+        /// <summary>
+        /// Recursively searches for a schedule for the courses in sections that has no conflicts
+        /// </summary>
+        /// <param name="courseToAdd">Indicates which index of sections we are trying to add a course from. In most cases
+        /// this should be the default value of 0 with the exception of recursive calls.</param>
+        /// <returns>boolean indicating if an acceptable schedule was found</returns>
+        private bool recursiveSearch(int courseToAdd=0)
+        {
+            if (foundSolution())
+                return true;
+            foreach(Course c in sections[courseToAdd])
+            {
+                if(safeToAdd(c))
+                {
+                    //push c onto the stack
+                    schedule.Add(c); 
+
+                    //the courseToAdd+1 will not cause an out of bounds index exception because foundSolution() will be true 
+                    //if the index is out of bounds and recursiveSearch will return true
+                    if(recursiveSearch(courseToAdd+1))
+                    {
+                        return true; //schedule is found!
+                    }
+                    else
+                    {
+                        //pop from the stack
+                        schedule.RemoveAt(schedule.Count - 1);
+                    }
+                }
+            }
+
+            return false; //unable to find a solution
+        }
+
+        /// <summary>
+        /// checks a course for time conflicts with the courses in the schedule
+        /// </summary>
+        /// <param name="c">course to check</param>
+        /// <returns>true if c has no conflicts and false otherwise</returns>
+        private bool safeToAdd(Course c)
+        {
+            foreach(Course scheduled in schedule)
+            {
+                if (Scheduler.haveOverlap(scheduled, c))
+                    return false;
+            }
+            return true;
+        }
+        private bool foundSolution()
+        {
+            scheduleFound = (sections.Count == schedule.Count);
+            return scheduleFound;
         }
 
 
