b0f8c6989d0046356b46be827c606f1abaaa74e2
Matt Smith <mjsmith92@outlook.com>
2017-04-13 22:57:51 -0400

    Start Project 5

---
+++ CS597_Project_5/CS597_Project_5/Search.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="CS597_Project_5.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Movie Search</h1>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblTerm" runat="server" Text="Search Term:"></asp:Label>
+    
+    &nbsp;&nbsp;
+        <asp:TextBox ID="tbxTerm" runat="server" Width="168px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+    
+    </div>
+        <br />
+    <div id="divResults" runat="server"></div>
+</body>
+</html>
---
+++ CS597_Project_5/CS597_Project_5/Search.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace CS597_Project_5
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            divResults.InnerHtml = "";
+
+            string requestUrl = buildRequestUrl();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUrl);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+        }
+
+        private string buildRequestUrl()
+        {
+            string stub = "http://www.omdbapi.com/";
+            string parms = "?s=" + Uri.EscapeDataString(tbxTerm.Text) + "&type=movie";
+            return stub + parms;
+        }
+    }
+}
