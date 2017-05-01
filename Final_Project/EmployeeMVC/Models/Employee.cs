ba4eadc6263e99f061be493031e8cc1f5ec3b570
Matt Smith <mjsmith92@outlook.com>
2017-04-30 23:43:11 -0400

    Final-Part 1: Add Employee model

---
+++ Final_Project/EmployeeMVC/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMVC.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public double Salary { get; set; }
+        [StringLength(1)]
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+        public string Performance { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
