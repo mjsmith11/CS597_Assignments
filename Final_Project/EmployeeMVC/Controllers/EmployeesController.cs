2f1780ee94e63dad4668ff12c2c0d08475cfa4ab
Matt Smith <mjsmith92@outlook.com>
2017-05-01 12:00:56 -0400

    Final-Part 1: Add searching ability

--- Final_Project/EmployeeMVC/Controllers/EmployeesController.cs
+++ Final_Project/EmployeeMVC/Controllers/EmployeesController.cs
@@ -15,9 +15,38 @@ namespace EmployeeMVC.Controllers
         private EmployeeDbContext db = new EmployeeDbContext();
 
         // GET: Employees
-        public ActionResult Index()
+        public ActionResult Index(string SearchBy, string SearchValue)
         {
-            return View(db.Employees.ToList());
+            var searchTypes = new List<string>();
+            searchTypes.Add("Last Name");
+            searchTypes.Add("First Name");
+            searchTypes.Add("Department");
+            searchTypes.Add("Location");
+            ViewBag.SearchBy = new SelectList(searchTypes);
+
+
+            var employees = from e in db.Employees select e;
+            if(!String.IsNullOrEmpty(SearchValue) && !String.IsNullOrEmpty(SearchBy))
+            {
+                if(SearchBy.Equals("Last Name"))
+                {
+                    employees = employees.Where(s => s.LastName.Contains(SearchValue));
+                }
+                else if(SearchBy.Equals("First Name"))
+                {
+                    employees = employees.Where(s => s.FirstName.Contains(SearchValue));
+                }
+                else if(SearchBy.Equals("Department"))
+                {
+                    employees = employees.Where(s => s.Department.Contains(SearchValue));
+                }
+                else if(SearchBy.Equals("Location"))
+                {
+                    employees = employees.Where(s => s.Location.Contains(SearchValue));
+                }
+            }
+
+            return View(employees);
         }
 
         // GET: Employees/Details/5
